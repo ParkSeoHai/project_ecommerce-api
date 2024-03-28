d40d5117a8d4877c91762245381027015ba58a23
Hai Nguyen <anhhai282003@gmail.com>
2024-03-28 15:38:18 +0700

    commit

--- project_ecommerce-api/obj/Debug/net8.0/project_ecommerce-api.AssemblyInfo.cs
+++ project_ecommerce-api/obj/Debug/net8.0/project_ecommerce-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_ecommerce-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0872b139474f642110ea191855664c7a9b12f733")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_ecommerce-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_ecommerce-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
